ba3424ec679656e716341effc743724d03d01886
Akash Vibhute <aroboticist@intrinsic.ai>
2024-10-02 09:51:03 +0800

    Update build-args.sh
    switch branch on rmf-web for url issue

--- dockerfiles/build-args.sh
+++ dockerfiles/build-args.sh
@@ -3,7 +3,7 @@
 ROS_DISTRO=jazzy
 : ${BUILDER_IMAGE:=localhost/rmf/builder:latest}
 RMF_INTERNAL_MSGS_COMMIT="main"
-RMF_WEB_COMMIT="debug/cluster"
+RMF_WEB_COMMIT="fix/url"
 RMF_BUILDING_MAP_MSGS_COMMIT="main"
 
 ARGS=$(getopt --options= --longoptions=ros-distro: --name="$0" -- "$@")
