dc8cc3cc879b28ca4b8b4d7262f5073cfbed2c5e
Akash Vibhute <aroboticist@intrinsic.ai>
2024-10-02 10:41:33 +0800

    Update build-args.sh
    use fix for authentication method

--- dockerfiles/build-args.sh
+++ dockerfiles/build-args.sh
@@ -3,7 +3,7 @@
 ROS_DISTRO=jazzy
 : ${BUILDER_IMAGE:=localhost/rmf/builder:latest}
 RMF_INTERNAL_MSGS_COMMIT="main"
-RMF_WEB_COMMIT="fix/url"
+RMF_WEB_COMMIT="fix/auth"
 RMF_BUILDING_MAP_MSGS_COMMIT="main"
 
 ARGS=$(getopt --options= --longoptions=ros-distro: --name="$0" -- "$@")
